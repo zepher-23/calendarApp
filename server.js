432da0f25c4d2ce8e2407e38e9ecd0143a040039
zepher-23 <30zepher@gmail.com>
2023-03-05 01:10:28 +0530

    edit 2

--- server.js
+++ server.js
@@ -86,4 +86,4 @@ app.post('/logout',(req,res)=> {
     res.sendStatus(200)
 })
 
-app.listen(5000,() =>{console.log("server running on port 5000")});
+app.listen(process.env.PORT || 5000,() =>{console.log("server running on port 5000")});
